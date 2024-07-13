51618b45ee1887a97735522fea2bb6aad0c8c709
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-07-13 23:44:18 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,38 @@
+
+// class 
+class User1 {
+    name = "faryal";
+    countryvisit = "turkey";
+
+}
+
+
+class User {
+    name : string
+    countryvisit : string
+
+
+constructor  (name1:"faryal",countryvisit1:"turkey") {
+
+
+
+
+    this.name = name1;
+    this.countryvisit = countryvisit1;
+} }
+
+
+// json
+
+const detail =  {
+    "name" : "faryal", "age" : 18}
+    console.log(detail["age"])
+
+
+    // const 
+    const a = 4
+    const b = 5
+    const c  = "best"
+
+
+    
